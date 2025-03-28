2a20e5ea8d47551edef1fef0983f9da2f2fe1510
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-03-28 12:56:53 -0500

    difficulty added

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
 
 const api = useAPI()
 const question = ref(null)
@@ -64,6 +65,7 @@ const shuffle = (array) => {
     </div>
 
   </div>
+  <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
 </div>
 <div v-else class="">
 Loading...
